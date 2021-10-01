f181338ce9597ab48abc2df1a9375d5dc99b3730
davidroman-hub <jobroman83@gmail.com>
2021-10-02 01:06:58 +0200

    [ADD] second request to get actorInfo

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,9 @@ import "./App.css";
 
 import axios from "axios";
 
-export const imageUp = async (file: any): Promise<any> => {
-  const datas = new FormData();
-  datas.append("file", file);
-  const res = await axios({
-    method: "POST",
-    url: `https://whois.nomada.cloud/upload`,
-    data: datas,
-    headers: {
-      "Content-Type": "application/json",
-      nomada: "ZWEzMjU1ZmItMDM2MS00YmI4LWJkNzQtMzEwNTk5OGFkZTc1",
-    },
-  });
-  console.log(res.data.actorName);
-  return res.data.actorName;
-};
-
-export const getActorInfo = async (name: string): Promise<any> => {
-  const res = await axios({
-    method: "GET",
-    url: `https://api.themoviedb.org/4/list/1`,
-    headers: {
-      "Content-Type": "application/json",
-      headers: {
-        Authorization: `Bearer 30db1237b9167f8afaf9e065b90d16b8`,
-      },
-    },
-  });
-  console.log(res.data.actorName);
-  return res.data.actorName;
-};
-
 const App = () => {
   const [file, setFile] = React.useState(null);
-  const [nombreActor, setNombreActor] = React.useState(null);
-  //const [actorName, setActorName] = React.useState(null);
+  const [info2, setInfo2] = React.useState(null)
 
   const onChange = (e: React.SyntheticEvent<EventTarget>): void => {
     const fileRef = (e.target as any).files[0];
@@ -57,10 +25,21 @@ const App = () => {
       },
     });
 
-    return setNombreActor(res.data.actorName);
+    const data = res.data.actorName;
+    return infoActor(data)
+
   };
 
-  if (!nombreActor) {
+  const infoActor = async (nombre: string): Promise<any> => {
+    const res = await axios({
+      method: "GET",
+      url: `https://api.themoviedb.org/3/search/person?api_key=30db1237b9167f8afaf9e065b90d16b8&language=en-US&query=${nombre}&page=1&include_adult=false`,
+    });
+
+    return setInfo2(res as any);
+  };
+
+  if (!info2) {
     return (
       <>
         <h1>No hay actor</h1>;
@@ -69,27 +48,8 @@ const App = () => {
       </>
     );
   } else {
-    return <h1>{nombreActor}</h1>;
+    return <h1>{JSON.stringify(info2)}</h1>
   }
-
-  // return (
-  //   <div
-  //     className="App"
-  //     style={{
-  //       display: "flex",
-  //       flexDirection: "column",
-  //       justifyContent: "center",
-  //       padding: 300,
-  //     }}
-  //   >
-  //     <h1>Analyze your Actor!!!</h1>
-  //     {`${nombreActor} <===============`}
-  //     <span>Or if you dont know him</span>
-  //     <span>drop a photo!</span>
-  //     <input type="file" onChange={onChange} />
-  //     <button onClick={() => imageUp(file)}>Search</button>
-  //   </div>
-  // );
 };
 
 export default App;
